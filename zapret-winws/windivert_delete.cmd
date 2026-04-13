ae9e8dd7ecf6315b1b5304356f52aa499a32bd8b
bol-van <none@none.none>
2026-04-13 10:42:23 +0300

    bundle

--- zapret-winws/windivert_delete.cmd
+++ zapret-winws/windivert_delete.cmd
@@ -2,8 +2,8 @@
 
 if "%1%" == "del" (
 	echo DELETE WINDIVERT DRIVER
-	sc stop windivert
 	sc delete windivert
+	sc stop windivert
 	goto :end
 )
 
@@ -14,7 +14,7 @@ echo.
 choice /C YN /M "Do you want to stop and delete windivert"
 if ERRORLEVEL 2 goto :eof
 
-"%~dp0elevator" "%~f0" del
+"%~dp0elevator" %0 del
 goto :eof
 
 :end
